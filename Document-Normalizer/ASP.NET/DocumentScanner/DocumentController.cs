0746e0ea0af9cb4b6ed641d4fbfc8e2786e31bb0
xulihang <xulihanghai@163.com>
2024-06-03 09:32:40 +0800

    init

---
+++ Document-Normalizer/ASP.NET/DocumentScanner/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentScanner
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+
+    }
+}
